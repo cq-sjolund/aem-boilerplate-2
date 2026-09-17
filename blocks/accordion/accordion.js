88715d1cba7160a65dc1da99d974fa3edf80b066
Rickard Sjölund <sjolund@adobe.com>
2026-09-17 11:30:55 +0200

    Created accordion block

---
+++ blocks/accordion/accordion.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+  const children = [];
+  [...block.children].forEach((row) => {
+    const details = document.createElement('details');
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...row.children[0].childNodes);
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
+    details.className = 'accordion-item';
+    details.append(summary, body);
+    children.push(details);
+  });
+
+  block.replaceChildren(...children);
+}
